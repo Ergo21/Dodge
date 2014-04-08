e035626b431d8998c89753c07006151f4ca6285b
Ergo21 <jww14@uni.brighton.ac.uk>
2014-04-08 20:09:45 +0100

    Attempted use of autobuild software

---
+++ src/Main.cpp
@@ -0,0 +1,16 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	cout << "Hello World" << endl;
+	
+	return 0;
+}
